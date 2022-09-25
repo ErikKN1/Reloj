0285846aa5ea242d1aefb5b9b4d429547fb7e084
ErikH <75594114+ErikKN1@users.noreply.github.com>
2022-09-24 21:30:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+const $Tiempo = document.querySelector( '.Tiempo'),
+$Fecha = document.querySelector('.Fecha');
+
+
+function DigitalClock() {
+    let f=new Date(),
+    dia=f.getDate(),
+    mes=f.getMonth()+1,
+    anio=f.getFullYear(),
+    diaSemana=f.getDay();
+
+    dia=('0'+dia).slice(-2);
+    mes=('0'+mes).slice(-2);
+
+    let timeString=f.toLocaleTimeString();
+    $Tiempo.innerHTML=timeString;
+    let semana=['SUN', 'MON','TUE','WED','THU','FRI','SAT'];
+    let showSemana=(semana[diaSemana]);
+    $Fecha.innerHTML= `${anio}-${mes}-${dia}  ${showSemana}` 
+    
+}
+
+setInterval(() => {
+    DigitalClock();
+
+}, 1000);
+
+const openEls = document.querySelectorAll("[data-open]");
+const closeEls = document.querySelectorAll("[data-close]");
+const isVisible = "is-visible";
+
+for (const el of openEls) {
+  el.addEventListener("click", function() {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
+
+for (const el of closeEls) {
+  el.addEventListener("click", function() {
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+    stop();
+  });
+}
+
+document.addEventListener("click", e => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+document.addEventListener("keyup", e => {
+  // if we press the ESC
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+    stop();
+});
+
+//JS Cronometro
+
+const stopwatch = document.getElementById('stopwatch');
+const playPauseButton = document.getElementById('play-pause');
+const secondsSphere = document.getElementById('seconds-sphere');
+
+let stopwatchInterval;
+let runningTime = 0;
+
+const playPause = () => {
+    const isPaused = !playPauseButton.classList.contains('running');
+    if (isPaused) {
+        playPauseButton.classList.add('running');
+        start();
+    } else {
+        playPauseButton.classList.remove('running');
+        pause();
+    }
+}
+
+const pause = () => {
+    secondsSphere.style.animationPlayState = 'paused';
+    clearInterval(stopwatchInterval);
+}
+
+const stop = () => {
+    secondsSphere.style.transform = 'rotate(-90deg) translateX(60px)';
+    secondsSphere.style.animation = 'none';
+    playPauseButton.classList.remove('running');
+    runningTime = 0;
+    clearInterval(stopwatchInterval);
+    stopwatch.textContent = '00:00';
+}
+
+const start = () => {
+    secondsSphere.style.animation = 'rotacion 60s linear infinite';
+    let startTime = Date.now() - runningTime;
+    secondsSphere.style.animationPlayState = 'running';
+    stopwatchInterval = setInterval( () => {
+        runningTime = Date.now() - startTime;
+        stopwatch.textContent = calculateTime(runningTime);
+    }, 1000)
+}
+
+const calculateTime = runningTime => {
+    const total_seconds = Math.floor(runningTime / 1000);
+    const total_minutes = Math.floor(total_seconds / 60);
+
+    const display_seconds = (total_seconds % 60).toString().padStart(2, "0");
+    const display_minutes = total_minutes.toString().padStart(2, "0");
+
+    return `${display_minutes}:${display_seconds}`
+}
